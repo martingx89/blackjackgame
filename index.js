6e55e3d4c7edf2e41d360053a9d3d56e5c7dac9d
Marcin Adamiak <marcin_89@outlook.com>
2022-12-29 15:29:37 +0100

    Add buttons

--- index.js
+++ index.js
@@ -2,22 +2,49 @@ let firstCard = 2 + Math.floor(Math.random() * 10);
 
 let secondCard = 2 + Math.floor(Math.random() * 10);
 
-const sum = firstCard + secondCard;
+let cards = [firstCard, secondCard];
 
-console.log(firstCard, secondCard);
+let hasBlackJack = false;
 
-if (sum <= 20) {
-	console.log("Do you want to draw a new card?");
-} else if (sum === 21) {
-	console.log("You have a BlackJack!");
-} else {
-	console.log("You'are out of the game!");
+let isAlive = true;
+
+let message = "";
+
+let sum = firstCard + secondCard;
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function startGame() {
+	cardsEl.textContent = "Cards: " + cards;
+	sumEl.textContent = "Sum: " + sum;
+
+	if (sum <= 20) {
+		message = "Do you want to draw a new card?";
+	} else if (sum === 21) {
+		message = "You have a BlackJack!";
+		hasBlackJack = true;
+	} else {
+		message = "You'are out of the game!";
+		isAlive = false;
+	}
+	messageEl.textContent = message;
 }
 
-let age = 22;
-
-if (age <= 21) {
-	console.log("You can not enter to the club");
-} else {
-	console.log("Welcome to the club");
+function newCard() {
+	let additionalCard = 2 + Math.floor(Math.random() * 10);
+	sum += additionalCard;
+	cards = [...cards, additionalCard];
+	startGame();
 }
+
+// let age = 22;
+
+// if (age <= 21) {
+// 	console.log("You can not enter to the club");
+// } else {
+// 	console.log("Welcome to the club");
+// }
+
+// console.log(hasBlackJack);
