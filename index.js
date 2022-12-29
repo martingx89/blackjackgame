de337693b587559bc0aaf8cf48a393ab64f09c8e
Marcin Adamiak <marcin_89@outlook.com>
2022-12-29 13:15:53 +0100

    Add some rules

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const sum = firstCard + secondCard;
 
 console.log(firstCard, secondCard);
 
-if (sum < 21) {
+if (sum <= 20) {
+	console.log("Do you want to draw a new card?");
 } else if (sum === 21) {
+	console.log("You have a BlackJack!");
 } else {
+	console.log("You'are out of the game!");
+}
+
+let age = 22;
+
+if (age <= 21) {
+	console.log("You can not enter to the club");
+} else {
+	console.log("Welcome to the club");
 }
