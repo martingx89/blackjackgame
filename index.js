5e4e291593cd8eb0152faea91ca699fd7afe8a7f
Marcin Adamiak <marcin_89@outlook.com>
2022-12-29 12:45:23 +0100

    Logic of game

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstCard = 2 + Math.floor(Math.random() * 10);
+
+let secondCard = 2 + Math.floor(Math.random() * 10);
+
+const sum = firstCard + secondCard;
+
+console.log(firstCard, secondCard);
+
+if (sum < 21) {
+} else if (sum === 21) {
+} else {
+}
